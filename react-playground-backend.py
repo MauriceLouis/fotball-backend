2bdbffea1974f346a315cda06ccfb529c274d95e
Louis Dieffenthaler <louis.maurice.dieffenthaler@nav.no>
2025-03-14 08:58:25 +0100

    minor changes to the backend

--- react-playground-backend.py
+++ react-playground-backend.py
@@ -8,7 +8,7 @@ source_csv = "favorite_meals.csv"
 file_exists = True if os.path.isfile(source_csv) else False
 if not file_exists:
     df = pd.DataFrame({"name": [], "favorite_meal": []})
-    df.to_csv(source_csv, mode="w", sep=";")
+    df.to_csv(source_csv, mode="w", sep=";", index=False)
     del df
 
 
@@ -19,13 +19,13 @@ CORS(app)
 @app.route('/api/data', methods=['POST'])
 def write_favorite_meal_for_person(name, meal):
 
-    ## This is a hacky way to update the table
+    ## This is a hacky way to update the table. Replace with Postgres
     df_temp = pd.DataFrame({"name": [name], "favorite_meal": [meal]})
     df = pd.read_csv(source_csv, sep=";")
     df = df[df["name"] != name].copy()
     df = pd.concat([df, df_temp])
 
-    df.to_csv(source_csv, mode="w", sep=";")
+    df.to_csv(source_csv, mode="w", sep=";", index=False)
 
     return jsonify({"message": f"Følgende er lagt inn: {name} sitt favorittmåltid er {meal}"})
 
