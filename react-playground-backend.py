4670110c4e35238aaf73dafe99075d4b9c255f31
Louis Dieffenthaler <louis.maurice.dieffenthaler@nav.no>
2025-03-14 08:44:24 +0100

    Added simple logic to the backend + updated the gitignore-file so that the csv-file is not part of the versioning

--- react-playground-backend.py
+++ react-playground-backend.py
@@ -1,9 +1,43 @@
+import os
+
+import pandas as pd
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+
+source_csv = "favorite_meals.csv"
+file_exists = True if os.path.isfile(source_csv) else False
+if not file_exists:
+    df = pd.DataFrame({"name": [], "favorite_meal": []})
+    df.to_csv(source_csv, mode="w", sep=";")
+    del df
+
+
 app = Flask(__name__)
 CORS(app)
+
+# The first method
+@app.route('/api/data', methods=['POST'])
+def write_favorite_meal_for_person(name, meal):
+
+    ## This is a hacky way to update the table
+    df_temp = pd.DataFrame({"name": [name], "favorite_meal": [meal]})
+    df = pd.read_csv(source_csv, sep=";")
+    df = df[df["name"] != name].copy()
+    df = pd.concat([df, df_temp])
+
+    df.to_csv(source_csv, mode="w", sep=";")
+
+    return jsonify({"message": f"Følgende er lagt inn: {name} sitt favorittmåltid er {meal}"})
+
+
+# The second method reads data from the source and returns the full table
 @app.route('/api/data', methods=['GET'])
-def get_data():
-    return jsonify({"message": "Hello from Python!"})
+def get_favorite_meals():
+    df = pd.read_csv(source_csv, sep=";")
+    json_msg = df.to_json()
+
+    return json_msg
+
+
 if __name__ == '__main__':
     app.run(debug=True)
