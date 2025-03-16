bf617986c0867a53b1237638caa25a2f6adf1729
Louis Dieffenthaler <louis.maurice.dieffenthaler@nav.no>
2025-03-16 21:46:50 +0100

    Lagt til en form med input og button som etter hvert skal snakke med skrive-delen av backenden

--- src/App.js
+++ src/App.js
@@ -4,6 +4,21 @@ import React, { useState, useEffect } from 'react';
 // Step 1: Add person`s name and favorite meal and write this to the csv-file before returning a message to the frontend
 // Step 2: Read the csv-file and show the people and their favorite meals :)
 
+function AddPersonsFavoriteMeal() {
+    return <form>
+        <div className="mb-4 align-middle" style={{display: 'flex', flexDirection: 'row'}}>
+        <label>
+            Personens navn: <input name="navn"/>
+        </label>
+            <label>
+                Favorittmåltid: <input name="favorite_meal"/>
+            </label>
+            <button type="submit"> Legg til!</button>
+        </div>
+    </form>
+
+}
+
 
 const App = () => {
     const [data, setData] = useState('');
@@ -22,9 +37,13 @@ const App = () => {
 
     return (
         <div>
-            <h1>React with Python</h1>
+            <h1>Our summary of people and food!</h1>
+            <h2>Report the person`s favorite meal</h2>
+            <AddPersonsFavoriteMeal></AddPersonsFavoriteMeal>
+
+            <h2>Summary (add a dashed line above this title)</h2>
             <ol>
-                {data.map(i=><li>{i.name + ": " + i.favorite_meal} </li>)}
+                {data && data.map(i=><li>{i.name + ": " + i.favorite_meal} </li>)}
             </ol>
 
         </div>
